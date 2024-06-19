92941510b13bd587d377344932e5247656283985
Swaleh Cosmas <69414682+Swaleh-developer@users.noreply.github.com>
2024-06-19 09:45:13 +0300

    Update Kepler's law.py

--- Kepler's law.py
+++ Kepler's law.py
@@ -1,3 +1,11 @@
+#Calculating the period of a satellite, the time required for a satellite to make a complete trip around the earch as determined by Kepler's law.
+# We define the period as a function of the distance of the satellite from the center of the earth.
+# Period = C x distance exp 1.5
+# C is a constant approx equal to 1/100. The period is in seconds and the distance in Kilometers
+
+#Determine the approximate distance in kilometers above the earth - user input
+#Set a radius of the earth which is a constant = 6378 km
+
 EARTH_RADIUS = 6378
 CONSTANT = 0.01
 SECONDS_PER_DAY = 86400
